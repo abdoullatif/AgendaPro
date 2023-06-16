742dd0bfb0be643831f84d76dd91a926a3ff67f1
Abdoulatif Souleymane Sooba <Abdoulatif@MacBook-Air-de-Abdoulatif.local>
2023-06-16 07:34:30 +0000

    troisiemes commit

--- app/Http/Controllers/GoogleAccountController.php
+++ app/Http/Controllers/GoogleAccountController.php
@@ -25,22 +25,22 @@ class GoogleAccountController extends Controller
             return redirect($google->createAuthUrl());
         }
 
-        // Use the given code to authenticate the user.
+        //
         $google->authenticate($request->get('code'));
         
-        // Make a call to the Google+ API to get more information on the account.
+        //
         $account = $google->service('Plus')->people->get('me');
 
         auth()->user()->googleAccounts()->updateOrCreate(
             [
-                // Map the account's id to the `google_id`.
+                // 
                 'google_id' => $account->id,
             ],
             [
-                // Use the first email address as the Google account's name.
+                // 
                 'name' => head($account->emails)->value,
                 
-                // Last but not least, save the access token for later use.
+                // 
                 'token' => $google->getAccessToken(),
             ]
         );
@@ -52,13 +52,12 @@ class GoogleAccountController extends Controller
     {
         $googleAccount->delete();
 
-        // Event though it has been deleted from our database,
-        // we still have access to $googleAccount as an object in memory.
+        // 
         $google->revokeToken($googleAccount->token);
 
         return redirect()->back();
     }
 
 
-    
+
 }
